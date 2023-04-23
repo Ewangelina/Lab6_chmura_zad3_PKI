943d43ff868184e38fa633178327a6467e3f831f
Ewangelina <235842@edu.p.lodz.pl>
2023-04-23 12:31:33 +0200

    revamp

--- index.js
+++ index.js
@@ -1,31 +1,75 @@
 const { google } = require('googleapis');
 const express = require('express')
 const OAuth2Data = require('./google_key.json')
+const axios = require('axios')
 
 const app = express()
-
+//https://lab6-zad3.onrender.com/auth/google/callback
 const CLIENT_ID = OAuth2Data.web.client_id; 
 const CLIENT_SECRET = OAuth2Data.web.client_secret; 
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
 
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 var authed = false;
 
-app.get('/', (req, res) => {
-    res.write(`<a href="https://lab6-zad3.onrender.com/google">Sign in with Google</a>`);
-    res.write(`<br>`);
+const clientID = "189a8fb8aff66b200a36";
+const clientSecret = "84ea18cd3fe49b8ea62f7417747c5a189359454a";
 
+app.set('view engine', 'ejs');
+var access_token = "";
+
+app.get('/auth/github/callback', (req, res) => {
+
+    // The req.query object has the query params that were sent to this route.
+    const requestToken = req.query.code
+    
+    axios({
+      method: 'post',
+      url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+      // Set the content type header, so that we get the response in JSON
+      headers: {
+           accept: 'application/json'
+      }
+    }).then((response) => {
+      access_token = response.data.access_token
+      res.redirect('/success');
+    })
+  })
+  
+  app.get('/success', function(req, res) {
+  
+    axios({
+      method: 'get',
+      url: `https://api.github.com/user`,
+      headers: {
+        Authorization: 'token ' + access_token
+      }
+    }).then((response) => {
+      res.render('pages/success',{ userData: response.data });
+    })
+  });
+
+app.get('/', (req, res) => {
+    var ret = "";
+    ret += `<a href="http://localhost:5000/google" target="_blank">Sign in with Google</a>`; //https://lab6-zad3.onrender.com/google
+    ret += `<br>`;
+    ret += `<a href="https://github.com/login/oauth/authorize?client_id=189a8fb8aff66b200a36" target="_blank">Sign in with Github</a>`;
+    ret += `<br>`;
+    ret += `<a href="http://localhost:5000/googleout" target="_blank">Sign OUT from Google</a>`;
+    res.send(ret)
 });
 
 app.get('/googleout', (req, res) => {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
     console.log('User signed out.');
+    authed = false;
+    res.write(`User signed out<br><a href="https://lab6-zad3.onrender.com">Homepage</a>`);
+    });
 });
 
 app.get('/google', (req, res) => {
-    if (!authed) 
-    {
+    if (!authed) {
         // Generate an OAuth URL and redirect there
         const url = oAuth2Client.generateAuthUrl({
             access_type: 'offline',
@@ -33,29 +77,19 @@ app.get('/google', (req, res) => {
         });
         console.log(url)
         res.redirect(url);
-    } 
-    else 
-    {
+    } else {
         var oauth2 = google.oauth2({auth: oAuth2Client, version: 'v2' });
         oauth2.userinfo.v2.me.get(function(err, result) {
-            if (err)
-            {
-                console.log('ERROR');
-                console.log(err);
-            }
+            if (err) return console.log('Returned an error: ' + err);
             else
             {
                 loggedUser = result.data.name;
-                console.log(loggedUser);
+                var ret = "Hello " + loggedUser;
+                ret += `<a href="http://localhost:5000/googleout" target="_blank">Sign in with Google</a>`;
             }
-
-            res.send('Logged in: '.concat(loggedUser, ' <img src = "', result.data.picture, '" height="23" width="23"> <br> <a href="#" onclick="signOut();">Sign out</a>'+
-            '<script> function signOut() { var auth2 = gapi.auth2.getAuthInstance(); auth2.signOut().then(function () {'+
-                  'console.log("User signed out."); window.location.replace("https://lab6-zad3.onrender.com/googleout");'+
-                '});}</script>'));
-        })
+        });
     }
-});
+})
 
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
