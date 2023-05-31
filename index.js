44ed15a96d76ab6d42b32881f30a5eb3e3d2aaf0
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-05-31 16:21:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.get('/temp', function (req, res)
           Dropdown button
         </button>
         <ul class="dropdown-menu">`;
-        client.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE'").then((error, response) => 
+        client.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public'").then((error, response) => 
         {
            
                 for (row in response)
